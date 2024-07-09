7022cbf705c2f7e40b6e60159ca192d5d8e4e66b
Julia Jurkowska <jurkowska.julia@gmail.com>
2024-07-09 19:51:15 +0200

    stream settings in config file

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+# EEG files
 FNAME_CALIBFILE = 'data/sample_calibfile.set'
 FNAME_REST = 'data/sample-eo-rest.xdf'
 FNAME_TASK = 'data/sample-task.xdf'
+
+# classification mode
+SAVE_MODEL = True
+SAVED_MODELS_PATH = 'models'
+
+# loading saved model
+MODEL_FNAME = 'Logistic_Regression_asr_2024-07-09_19-19-31.pkl'
+
+# streaming
+FIF_FNAME = ''
+SUBJECT = ''
+CONDITION = ''
+STREAM_TIME = 31
+INTERVAL = 10

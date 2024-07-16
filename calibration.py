14a838b54f479bc05429c34d7bc8a17e8b37a245
Krzysztof Tołpa <82715129+krzysztoftolpa@users.noreply.github.com>
2024-07-16 19:25:14 +0200

    added complexity measures

--- calibration.py
+++ calibration.py
@@ -2,7 +2,7 @@ import mne
 import pandas as pd
 from meegkit.asr import ASR
 
-from power_features import calculate_powers_xdf
+from power_features import calculate_powers_xdf, calculate_complexity_xdf
 import config
 
 
@@ -18,11 +18,18 @@ def asr_calibration(fname_calibfile: str = config.FNAME_CALIBFILE):
 
 def asr_for_condition(asr, fname: str, condition: str):
     results = calculate_powers_xdf(fname, asr)
-    df_results = pd.DataFrame(results,
+    df_powers = pd.DataFrame(results,
                               columns=['channel', 'delta', 'delta_rel', 'theta', 'theta_rel', 'alpha', 'alpha_rel',
                                        'beta', 'beta_rel', 'gamma', 'gamma_rel'])
+    
+    complexity_results = calculate_complexity_xdf(fname, asr)
+    
+    df_complexity = pd.DataFrame(complexity_results, columns=['channel', 'sampen', 'appen', 'higuchi', 'katz'])
+    
+    df_results = pd.concat([df_powers, df_complexity], axis=1)
     df_results.insert(0, 'condition', condition)
 
+   
     return df_results
 
 

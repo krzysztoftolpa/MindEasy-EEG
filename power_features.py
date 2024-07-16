e01712a4a3fbdcf4516fcf9397791285729439ed
Krzysztof Tołpa <82715129+krzysztoftolpa@users.noreply.github.com>
2024-07-16 19:24:36 +0200

    Added complexity measures

--- power_features.py
+++ power_features.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 import pyxdf
 import mne
-
+from mne_features.univariate import (compute_samp_entropy, compute_app_entropy, compute_higuchi_fd, compute_katz_fd)
 
 def bandpower(data, sf, band, window_sec=None, relative=False):
     """Compute the average power of the signal x in a specific frequency band.
@@ -129,5 +129,69 @@ def calculate_powers_epoch(epoch, sfreq, channels, asr):
         gamma_rel = bandpower(chan, sfreq, [30, 40], 1, relative=True)
 
         results.append([channels[idx], delta, delta_rel, theta, theta_rel, alpha, alpha_rel, beta, beta_rel, gamma, gamma_rel])
+
+    return results
+
+
+
+def calculate_complexity_epoch(epoch, channels, asr):
+
+   
+    epoch = asr.transform(epoch)
+
+
+    sampen = compute_samp_entropy(epoch, emb=2)
+    appen = compute_app_entropy(epoch, emb = 2)
+    higuchi = compute_higuchi_fd(epoch)
+    katz = compute_katz_fd(epoch)
+    
+    metrics = np.vstack((sampen, appen, higuchi, katz)).T
+    results = np.hstack((np.array(channels).reshape(-1, 1), metrics))
+    return results
+
+
+
+
+
+def calculate_complexity_xdf(fname, asr):
+
+    channels = ['TP9', 'AF7', 'AF8', 'TP10']
+
+    # load data
+    streams, header = pyxdf.load_xdf(fname)
+    data = streams[0]["time_series"].T
+    data = data[:-1,:]  # last channel is AUX
+    data = data*1e-6  # convert to V
+    sfreq = float(streams[0]["info"]["nominal_srate"][0])
+    info = mne.create_info(channels, sfreq, 'eeg')
+    raw = mne.io.RawArray(data, info)
+    raw.set_montage('standard_1020')
+
+    # filter data
+    raw = raw.filter(l_freq=0.5, h_freq = 40)
+
+    # make fixed length epochs
+    epochs = mne.make_fixed_length_epochs(raw, duration = 1.0, overlap = 0)
+    # epochs.plot()
+
+    results = []
+    for epoch in epochs:
+
+        # plt.plot(epoch[0,:])
+        epoch = asr.transform(epoch)
+        # plt.plot(epoch[0,:])
+        # plt.show()
+        sampen = compute_samp_entropy(epoch, emb=2)
+        appen = compute_app_entropy(epoch, emb = 2)
+        higuchi = compute_higuchi_fd(epoch)
+        katz = compute_katz_fd(epoch)
+        
+        metrics = np.vstack((sampen, appen, higuchi, katz)).T
+        tmp_results = np.hstack((np.array(channels).reshape(-1, 1), metrics))
+
+
+        results.append(tmp_results)
+
+    results = np.vstack(results)
 
     return results

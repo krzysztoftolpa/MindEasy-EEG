921bc45632d7c6a4b4381895fdb01db80328512c
Krzysztof Tołpa <82715129+krzysztoftolpa@users.noreply.github.com>
2024-07-17 17:21:50 +0200

    extracting epochs data

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
-
+import mne
+import pyxdf
+import numpy as np
 
 def balance_conditions(df, condition_col):
     """
@@ -41,3 +43,51 @@ def generate_eeg_metrics_json(file_path, values):
         json.dump(data, json_file, indent=4)
 
     print(f"JSON file created at: {file_path}")
+
+
+
+def extract_epochs_array(fname, asr):
+
+    channels = ['TP9', 'AF7', 'AF8', 'TP10']
+
+    # load data
+    streams, header = pyxdf.load_xdf(fname)
+    data = streams[0]["time_series"].T
+    data = data[:-1,:]  # last channel is AUX
+    data = data*1e-6  # convert to V
+    sfreq = float(streams[0]["info"]["nominal_srate"][0])
+    info = mne.create_info(channels, sfreq, 'eeg')
+    raw = mne.io.RawArray(data, info)
+    raw.set_montage('standard_1020')
+
+    # filter data
+    raw = raw.filter(l_freq=0.5, h_freq = 40)
+
+    # make fixed length epochs
+    epochs = mne.make_fixed_length_epochs(raw, duration = 1.0, overlap = 0)
+    # epochs.plot()
+
+    results = []
+    for epoch in epochs:
+
+        # plt.plot(epoch[0,:])
+        epoch = asr.transform(epoch)
+
+        results.append(epoch)
+
+    return np.array(results)
+
+
+def get_muse_info(fname):
+    channels = ['TP9', 'AF7', 'AF8', 'TP10']
+
+    # load data
+    streams, header = pyxdf.load_xdf(fname)
+    data = streams[0]["time_series"].T
+    data = data[:-1,:]  # last channel is AUX
+    data = data*1e-6  # convert to V
+    sfreq = float(streams[0]["info"]["nominal_srate"][0])
+    info = mne.create_info(channels, sfreq, 'eeg')
+    info.set_montage('standard_1020')
+
+    return info
